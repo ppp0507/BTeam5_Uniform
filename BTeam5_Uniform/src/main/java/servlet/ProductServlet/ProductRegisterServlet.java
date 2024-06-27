9a484a2f1dcf3ca54f7ae98b1cf0165f6c69aabf
ppp0507 <parkin0507@korea.ac.kr>
2024-06-27 11:54:44 +0900

    ProductRegisterServlet エラー修正

--- BTeam5_Uniform/src/main/java/servlet/ProductServlet/ProductRegisterServlet.java
+++ BTeam5_Uniform/src/main/java/servlet/ProductServlet/ProductRegisterServlet.java
@@ -13,45 +13,44 @@ import jakarta.servlet.http.HttpServletResponse;
 public class ProductRegisterServlet extends HttpServlet {
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException{
-		
+			throws ServletException, IOException {
+
 		// エラー文を格納する変数
 		String error = "";
 		String name = null;
-		int price =0;
-		int stock =0;
-		
+		int price = 0;
+		int stock = 0;
+
 		// 商品へのアクセスを提供するDAO
 		ProductDAO productDao = new ProductDAO();
-		
+
 		try {
-			
+
 			// フォームから送信された値を取得
 			name = request.getParameter("name");
 			price = Integer.parseInt(request.getParameter("price"));
 			stock = Integer.parseInt(request.getParameter("stock"));
-			
-	
-				// 画像の登録機能は実装していないのでNULLを代わりに代入
-				productDao.insert(name, price, stock, null);
 
-			
+			if (name.equals("")) {
+				error = "商品名を入力してください。";
+				return;
+			} else if (price == 0) {
+				error = "金額を入力してください。";
+				return;
+			} else if (stock == 0) {
+				error = "個数を入力してください。";
+				return;
+			}
+
+			// 画像の登録機能は実装していないのでNULLを代わりに代入
+			productDao.insert(name, price, stock, null);
+
 		} catch (IllegalStateException e) {
 			// エラー処理
 			error = "DB接続エラーの為、一覧表示はできませんでした。";
 			e.printStackTrace();
 		} catch (Exception e) {
-			if(name.equals("")) {
-
-				error = "商品名を入力してください。";
-				return;
-			}else if(price == 0){
-				error = "金額を入力してください。";
-				return;
-			}else if(stock == 0){
-				error = "個数を入力してください。";
-				return;
-			}
+			error = "入力値が正しくありません。";
 		} finally {
 			if (error.length() == 0) {
 				// フォワード処理
@@ -63,6 +62,6 @@ public class ProductRegisterServlet extends HttpServlet {
 				request.getRequestDispatcher("/view/Common/error.jsp").forward(request, response);
 			}
 		}
-		
+
 	}
 }
